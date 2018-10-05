58db501af14d08e66f3aa09214a1954a345366a6
Deryk <derahner@outlook.com>
2018-10-04 20:39:42 -0700

    Added 3d plotting

--- GrindingCalcs.py
+++ GrindingCalcs.py
@@ -33,11 +33,25 @@ F_pass = F_thick/F_pd             #Number of passes for the finishing region
 
 Pass_tot = S_pass+R_pass+F_pass   #Total number of passes required
 
+"""Equation fitting"""
+a = np.polyfit(S_pass,F_pass,R_pass)
+
+"""Printing"""
+
+
 """Plotting"""
 plt.plot(R_pd,Pass_tot)
 plt.xlabel("Roughing Pass Depth")
 plt.ylabel("Number of Passes")
 plt.title("Roughing Pass Depth vs. # of passes")
+plt.show()
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(S_pass,F_pass,R_pass, marker='*')
+ax.set_xlabel("Scale Passes")
+ax.set_zlabel("Roughing Passes")
+ax.set_ylabel("Finishing Passes")
 plt.show()
 
 
