b363ad9c192109c33a4cf71205dfdec3c47ed1f6
Deryk <derahner@outlook.com>
2018-10-15 21:31:34 -0700

    Tried adding percentages and simpflied model

---
+++ GrindingCalcs2.py
@@ -0,0 +1,5 @@
+"""This is an approximation derived from the orginal model. The intention of this model is to allow for much simpler 2D analysis"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
